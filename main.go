10c9c8312d3626493edb180131431d538977ece3
mikujo <jo.tomorrow.0405@gmail.com>
2022-06-28 08:02:59 +0900

    create main.go

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	controller "go-test-app/controllers"
+)
 
 func main() {
-	fmt.Printf(("Hello world"))
+	serve()
+}
+
+func serve() {
+	router := gin.Default()
+
+	/* 静的ファイルのパス指定 */
+	router.Static("/views", "./views")
+
+	/* ルーターの指定　URLへのアクセスに対して静的ページを返す */
+	router.StaticFS("/go-test", http.Dir("./views/static"))
+	/* 全ての商品情報のJSONを返す */
+	router.GET("/fetchAllProducts", controller.FetchAllProducts)
+	/* 1つの商品情報の状態のJSONを返す */
+	router.GET("/fetchProduct", controller.FindProduct)
+	/* 商品情報を検索する */
+	router.GET("/searchProduct", controller.SearchProduct)
+	/* 商品情報をDBへ登録する */
+	router.POST("/addProduct", controller.AddProduct)
+	/* 商品情報の状態を変更する */
+	router.POST("/updateProduct", controller.UpdateProduct)
+	/* 商品情報を削除する */
+	router.POST("/deleteProduct", controller.DeleteProduct)
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal("Server Run Failed.", err)
+	}
 }
